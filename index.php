6115b5c50c9965da2da19ed172a6af4f54dd6b65
shopthanhhuy <huy656363@gmail.com>
2023-01-27 20:02:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>  
+<html>  
+<head>  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initital-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+<title>web phu kien</title>  
+</head>  
+<body>  
+    <div class="wrapper">
+        <div class="header">
+        </div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>    
+            </ul>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+            <ul class="list_sidebar">
+                <li><a href="#">Phụ kiện</a></li>
+                <li><a href="#">Tai nghe</a></li>
+                <li><a href="#">Củ sạc</a></li>
+                <li><a href="#">Dán màn hình</a></li>
+                <li><a href="#">Dán màn hình</a></li>    
+                <li><a href="#">Gậy tự sướng</a></li>    
+                <li><a href="#">Lót bàn phím </a></li>    
+
+            </ul>
+            </div>
+            <div class="main_content">
+                main content
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="footer">
+            <p>header</p>
+        </div>
+    </div>  
+</body>  
+</html>  
